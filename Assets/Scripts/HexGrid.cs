74c7abcad59fb643957efa4acf64fd018c33ec6d
Alex <amr_sauron@msn.com>
2017-03-02 21:41:14 +0100

    Playing around with hexgrid

---
+++ Assets/Scripts/HexGrid.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HexGrid : MonoBehaviour {
+
+	public GameObject Hex;
+	// Grid width in hexes
+	public int gridWidth = 11;
+	public int gridHeight = 11;
+
+	private float hexWidth;
+	private float hexHeight;
+
+	void setHexSizes () 
+	{
+		hexWidth = Hex.GetComponent<Renderer> ().bounds.size.x;
+		hexHeight = Hex.GetComponent<Renderer> ().bounds.size.y;
+	}
+
+	Vector3 calcInitialPos ()
+	{
+		Vector3 initialPos;
+		initialPos = new Vector3 (-hexWidth * gridWidth / 2f + hexWidth / 2, gridHeight / 2f * hexHeight / 2, 0);
+		return initialPos;
+	}
+
+	void createHex(Vector3 pos)
+	{
+		GameObject thisHex = (GameObject)Instantiate (Hex);
+		thisHex.transform.position = pos;
+	}
+
+	public Vector3 calcUnityCoord(Vector2 gridPos)
+	{
+		//Position of the first tile
+		Vector3 initPos = calcInitialPos();
+		float x = initPos.x + gridPos.x * hexWidth;
+		float y = initPos.y - gridPos.y * hexHeight;
+		return new Vector3 (x, y, 0);
+	}
+
+	void createHexGrid()
+	{
+		GameObject hexGridObject = new GameObject ("HexGrid");
+		hexGridObject.transform.parent = this.transform;
+
+		for (int y = 0; y < gridHeight; y++) 
+		{
+			for (int x = 0; x < gridWidth; x++) 
+			{
+				GameObject thisHex = (GameObject) Instantiate(Hex);
+				Vector2 gridPos = new Vector2(x, y);
+				thisHex.transform.position = calcWorldCoord(gridPos);
+				thisHex.transform.parent = hexGridObject.transform;
+			}
+		}
+	}
+
+	public Vector3 calcWorldCoord(Vector2 gridPos)
+	{
+		//Position of the first tile
+		Vector3 initPos = calcInitialPos();
+		float xoffset = 0;
+		float yoffset = 0;
+		// If the row number is a multiple of 2
+		if (gridPos.y % 2 != 0)
+			// The x offset is equal to the radius of the inner circle, or half the width for pointy hexes
+			xoffset = hexWidth / 2;
+
+		float x = initPos.x + xoffset + gridPos.x * hexWidth;
+		// Every new line is offset in y direction by 3/4 of the outer circle diameter, or hexagon height
+		yoffset = 0.85f;
+		float y = initPos.y - gridPos.y * hexHeight * yoffset;
+		return new Vector3(x, y, 0);
+	}
+
+	// Use this for initialization
+	void Start () 
+	{
+		setHexSizes ();
+		createHexGrid ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
