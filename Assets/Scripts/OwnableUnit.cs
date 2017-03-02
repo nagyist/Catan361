f93ed7382454dff7fc03a042a0178dc30616de4e
Alex <amr_sauron@msn.com>
2017-03-02 13:15:04 +0100

    Minor alterations

--- Assets/Scripts/OwnableUnit.cs
+++ Assets/Scripts/OwnableUnit.cs
@@ -9,11 +9,11 @@ public abstract class OwnableUnit
     // 1. abstract class doesn't need a constructor
     // 2. changed player attribute for well-formedness
     
-<<<<<<< HEAD
+//<<<<<<< HEAD
     Player player { get; set; }
-=======
+//=======
     Player owner { get; set; }
->>>>>>> origin/master
+//>>>>>>> origin/master
     
 
 }
