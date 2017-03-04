fbd777c4e2e77a02595bdd69c2dedbc19bac0c5c
Jeremie <jeremie@weboxigen.com>
2017-03-04 14:27:42 -0500

    merge

---
+++ Assets/GUIInterface.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class GUIInterface : MonoBehaviour {
+    private Canvas guiCanvas;
+
+    public IEnumerator ShowMessage(string msg, float delay = 1.75f) {
+        Text guiText = guiCanvas.GetComponent<Text>();
+        guiText.text = msg;
+        guiText.enabled = true;
+        yield return new WaitForSeconds(delay);
+        guiText.enabled = false;
+    }
+
+	// Use this for initialization
+	void Start () {
+        guiCanvas = GameObject.FindObjectOfType<Canvas>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+    private HexGrid gameBoard;
+    private GamePlayer currentPlayer;
+    private GUIInterface gui;
+    
+    void Awake () {
+        gameBoard = GetComponent<HexGrid>();
+        currentPlayer = GetComponent<GamePlayer>();
+        gui = GetComponent<GUIInterface>();
+    }
+
+    public void RollDice() {
+        int diceResult = Random.Range(1, 6);
+        StartCoroutine(gui.ShowMessage("Player X rolled " + diceResult));
+    }
+
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
