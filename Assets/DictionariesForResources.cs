be08f56bfa5b97598a71f4c4b21f563428456dcd
Alex <amr_sauron@msn.com>
2017-03-05 17:58:41 -0500

    Some Assets to Play Around With

---
+++ Assets/DictionariesForResources.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class DictionariesForResources : MonoBehaviour {
+
+	public static Dictionary<String, Texture2D> resourceTextureList = new Dictionary<String, Texture2D> ();
+
+	public void AddTexture(String num)
+	{
+		Texture2D texture = (Texture2D)Resources.Load ("GoodDirt.psd");
+		resourceTextureList.Add(num, texture);
+	}
+
+	// Use this for initialization
+	void Start () {
+		AddTexture ("1");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
