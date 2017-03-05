98820d23af2d90812a773fec15ce9feeccc4231f
Jeremie Poisson <jeremie@weboxigen.com>
2017-03-04 21:58:19 -0500

    removed test

--- Assets/Scripts/EdgeCollection.cs
+++ Assets/Scripts/EdgeCollection.cs
@@ -24,8 +24,6 @@ public class EdgeCollection {
 		string key = (firstVal < secondVal ? flattenFirst + flattenSecond : flattenSecond + flattenFirst);
 		if (!Edges.ContainsKey (key)) {
 			Edges.Add (key, edgeToAdd);
-		} else {
-			Debug.Log ("test");
 		}
 	}
 
