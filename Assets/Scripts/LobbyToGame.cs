d0dbbab67fe41f28c90669f709f3603881e7bee4
Dennis Kuang <denniskrq@hotmail.com>
2017-03-06 17:35:59 -0500

    Player prefab created, lobby-to-game info passing started
    - Created "Player" prefab in the root of the project, has "GamePlayer"
    script attached to hold relevent information, and "NetworkIdentity"
    attached to support functionalities
    - Elementary info passing from Lobby into Game possible: Players are
    instantiated as clones into the Game scene upon load, and name & color
    from lobby are passed onto the instantiated objects

---
+++ Assets/Scripts/LobbyToGame.cs
@@ -0,0 +1,18 @@
+﻿using Prototype.NetworkLobby;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class LobbyToGame : LobbyHook {
+
+    public override void OnLobbyServerSceneLoadedForPlayer(NetworkManager manager, GameObject lobbyPlayer, GameObject gamePlayer)
+    {
+        base.OnLobbyServerSceneLoadedForPlayer(manager, lobbyPlayer, gamePlayer);
+        LobbyPlayer lobby = lobbyPlayer.GetComponent<LobbyPlayer>();
+        GamePlayer game = gamePlayer.GetComponent<GamePlayer>();
+
+        game.myColor = lobby.playerColor;
+        game.myName = lobby.playerName;
+    }
+}
