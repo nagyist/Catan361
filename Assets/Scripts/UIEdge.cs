ad1ff90f41bcb5e0823f9f80bd028b04b22adfd6
abehnan <abehnan@gmail.com>
2017-03-07 23:36:52 -0500

    Enhanced color quality + notes on buying settlement/road for demo
    Intersections and edges can now be changed to a set color after being
    clicked. This can be used to represent buying settlements/roads.

--- Assets/Scripts/UIEdge.cs
+++ Assets/Scripts/UIEdge.cs
@@ -2,30 +2,46 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+// note: more development needed on OnMouseDown function
+//          in order to represent buying a settlement
+// for test purposes assume current player's color is cyan
 public class UIEdge : MonoBehaviour {
 
 	public Edge referencedEdge;
 
     SpriteRenderer edgeRenderer;
 
+
+
 	void OnMouseEnter() {
-		GetComponent<SpriteRenderer> ().color = Color.red;
+		edgeRenderer.color = Color.yellow;
 
 	}
 
+    
 	void OnMouseExit() {
-		GetComponent<SpriteRenderer> ().color = Color.black;
+        if (edgeRenderer.color != Color.cyan)
+            edgeRenderer.color = Color.black;
 	}
+    
 
+
+
+    // not complete
+    // we can change the OnMouseDown function to represent buying a road
+    // we can just change the color of the edge to that of the player
+    // waiting until concept of player can be fully utlized
     private void OnMouseDown()
     {
-        GetComponent<SpriteRenderer>().color = Color.cyan;
+        edgeRenderer.color = Color.cyan;
     }
 
     // Use this for initialization
     void Start () {
         edgeRenderer = GetComponent<SpriteRenderer>();
         edgeRenderer.sortingLayerName = "Edge";
+        edgeRenderer.color = Color.black;
 	}
 	
 	// Update is called once per frame
--- Assets/Scripts/UIIntersection.cs
+++ Assets/Scripts/UIIntersection.cs
@@ -2,6 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+// note: more development needed on OnMouseDown function
+//          in order to represent buying a settlement
+// for test purposes assume current player's color is cyan
+
+
 public class UIIntersection : MonoBehaviour {
 
 	public Intersection referencedIntersection;
@@ -9,19 +15,35 @@ public class UIIntersection : MonoBehaviour {
     private SpriteRenderer intersectionRenderer;
 
 	void OnMouseEnter() {
-		GetComponent<SpriteRenderer> ().color = Color.blue;
-
+        intersectionRenderer.color = Color.yellow;
 	}
 
+    
 	void OnMouseExit() {
-		GetComponent<SpriteRenderer> ().color = Color.red;
+        // check to see if intersection is owned
+        if (intersectionRenderer.color != Color.cyan)
+		    intersectionRenderer.color = Color.red;
 	}
+    
 
 	// Use this for initialization
 	void Start () {
         intersectionRenderer = GetComponent<SpriteRenderer>();
         intersectionRenderer.sortingLayerName = "Intersection";
+        intersectionRenderer.color = Color.red;
 	}
+
+    // not complete
+    // we can change the OnMouseDown function to represent buying a settlement
+    // we can just change the color of the intersection to that of the player
+    // waiting until concept of player can be fully utlized
+    private void OnMouseDown()
+    {
+        intersectionRenderer.color = Color.cyan;
+    }
+
+    // Update is called once per frame
+    
 
 	// Update is called once per frame
 	void Update () {
