02a09d04c7360b90b205fd0eea7e2e5abc76539e
abehnan <abehnan@gmail.com>
2017-03-21 13:05:32 -0400

    Added comments to GameManager and ResourcePanel
    Also changed the error message when the game hasn't started yet to the
    resource panel

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,29 +3,43 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
 
+/* this class is used to manage:
+ *      1. all of the GamePlayer game objects
+ *      2. the current game's state
+ *      3. the rolldice function
+ */
+
 public class GameManager : Singleton<GameManager> {
+
+    // initialize some variables
 	public static GameObject LocalPlayer = null;
-	public static List<GameObject> ConnectedPlayers = new List<GameObject> ();
-	public static Dictionary<string, GameObject> ConnectedPlayersByName = new Dictionary<string, GameObject> ();
 	public static GameState CurrentGameState = null;
 	public static int PlayerCount = 0;
-
 	public GUIInterface gui;
 
-	protected GameManager() {
+    // note: players are added in two different data structures
+    // the ConnectedPlayers List is a list containing all the game objects
+    public static List<GameObject> ConnectedPlayers = new List<GameObject>();
+    // the ConnectedPlayersByName is a dictionary linking all of the connected players to a given name: Player1, Player2, etc..
+    public static Dictionary<string, GameObject> ConnectedPlayersByName = new Dictionary<string, GameObject>();
+
+    protected GameManager() {
 		
 	}
 
+    // awake function used to initialize variables before the game starts
     void Awake () {
         gui = GetComponent<GUIInterface>();
     }
 
+    // this function will add the player argument to the GameManager's data structures
 	public static void PlayerConnected(GameObject player) {
 		player.GetComponent<GamePlayer> ().myName = "Player" + (++PlayerCount);
 		ConnectedPlayers.Add (player);
 		ConnectedPlayersByName.Add (player.GetComponent<GamePlayer> ().myName, player);
 	}
 
+    // this function sets the argument as the GameManager's local player 
 	public static void SetLocalPlayer(GameObject player) {
 		LocalPlayer = player;
 	}
@@ -34,67 +48,95 @@ public class GameManager : Singleton<GameManager> {
         
     }
 
+    // this function returns true is the current game's state isn't null
 	public bool GameStateReady() {
 		return CurrentGameState != null;
 	}
 
+    // returns the current game's state
 	public GameState GetCurrentGameState() {
 		return CurrentGameState;
 	}
 
+    // returns true is the game state is ready for the stage defined by the status argument
 	public bool GameStateReadyAtStage(GameState.GameStatus status) {
 		return GameStateReady () && GetCurrentGameState ().CurrentStatus >= status;
 	}
 
+    // this function is used when a player wishes to take their turn
 	public bool CurrentPlayerTakeTurn() {
+        // if the player isn't allowed to take their turn, return false
 		if (!GameManager.Instance.GetCurrentGameState ().CurrentTurn.IsLocalPlayerAllowedToTakeTurn()) {
 			StartCoroutine (GameManager.Instance.gui.ShowMessage ("Cannot take turn"));
 			return false;
 		}
 
+        // if the player can take their turn, they can place a road and a settlment
 		if (GameManager.Instance.GetCurrentGameState ().CurrentTurn.IsInSetupPhase ()) {
 			GameManager.LocalPlayer.GetComponent<GamePlayer> ().placedRoad = false;
 			GameManager.LocalPlayer.GetComponent<GamePlayer> ().placedSettlement = false;
 		} 
 
+        // tells the local player to take their turn 
 		LocalPlayer.GetComponent<GamePlayer> ().CmdTakeTurn ();
 		return true;
 	}
 
+    // this function is used when a player wishes to end their turn
 	public bool CurrentPlayerEndTurn() {
+
+        // if the player isn't currently taking their turn, show error and return false
 		if (!GameManager.Instance.GetCurrentGameState ().CurrentTurn.IsLocalPlayerTurn ()) {
 			StartCoroutine (GameManager.Instance.gui.ShowMessage ("Cannot end turn"));
 			return false;
 		}
 
+        // calls the local player's cmd function to end their turn
 		LocalPlayer.GetComponent<GamePlayer> ().CmdEndTurn ();
 		return true;
 	}
 
-	public bool RollDice(int roll) {
-		if (!GameManager.Instance.GameStateReadyAtStage (GameState.GameStatus.GRID_CREATED)) {
+    // this function is used update player resources accordingly
+    public bool RollDice(int roll) {
+
+        GamePlayer player = LocalPlayer.GetComponent<GamePlayer>();
+
+        // returns false if game isn't ready
+        if (!GameManager.Instance.GameStateReadyAtStage (GameState.GameStatus.GRID_CREATED))
 			return false;
-		}
 
-		/**
-		 * Figure out edge colletion bug
-		 * Different color for edges/intersections
-		 * */
+		/* 
+         * TODO:
+		 *      Figure out edge colletion bug
+		 *      Different color for edges/intersections
+         *      Give the resources to all of the players (easy fix will do later)
+		 * 
+         */
 
-		// update player resources accordingly
-		GamePlayer player = LocalPlayer.GetComponent<GamePlayer> ();
+        // go through all the intersections
 		foreach (string key in GameManager.Instance.GetCurrentGameState().CurrentIntersections.Intersections.Keys) {
 			Intersection i = GameManager.Instance.GetCurrentGameState ().CurrentIntersections.Intersections [key];
+
+            // if the intetersecion has a settlement and that settlement is owned by teh local player
 			if (i.SettlementCount > 0 && i.SettlementOwner == player.myName) {
+                // go through all the adjacent hexes
 				List<Vec3> adjHexes = new List<Vec3> (new Vec3[] { i.adjTile1, i.adjTile2, i.adjTile3 });
 				foreach (Vec3 hex in adjHexes) {
 					HexTile tile = GameManager.Instance.GetCurrentGameState ().CurrentBoard [hex];
+
+                    // continue if the tile number's don't match the rolled number
 					if (tile.IsWater) { continue; }
 					if (tile.SelectedNum != roll) { continue; }
 
-					if (player.playerResources.ContainsKey (tile.Resource)) {
+                    // to to see if the player's resrouces already contain the key for the resource
+                    // if so then add the appropriate amount of resources
+					if (player.playerResources.ContainsKey (tile.Resource))
+                    {
 						player.playerResources [tile.Resource] = player.playerResources [tile.Resource] + i.SettlementCount;
-					} else {
+					}
+                    // if the player's resource don't already contain the key for the resource then add it
+                    else
+                    {
 						player.playerResources.Add (tile.Resource, i.SettlementCount);
 					}
 				}
--- Assets/Scripts/UI/PlayerResourcePanel.cs
+++ Assets/Scripts/UI/PlayerResourcePanel.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+
+// this class is used to control the text displayed in the left pnale showing all the players' resources
 public class PlayerResourcePanel : MonoBehaviour {
 
 	// Use this for initialization
@@ -10,6 +12,7 @@ public class PlayerResourcePanel : MonoBehaviour {
 		
 	}
 
+    // this function prints out the local player's resources
 	string ResDisplay(GamePlayer player, StealableType type) {
 		if (player.playerResources.ContainsKey (type)) {
 			return "" + player.playerResources [type];
@@ -21,42 +24,39 @@ public class PlayerResourcePanel : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-
+        // check if the game is ready  (if the grid is created)
 		if (GameManager.Instance.GameStateReadyAtStage (GameState.GameStatus.GRID_CREATED)) {
 
+            //initialiaze variables
             string buffer;
 			GamePlayer currentPlayer = GameManager.LocalPlayer.GetComponent<GamePlayer> ();
+            List<GameObject> connectedPlayers = GameManager.ConnectedPlayers;
 
+            // get the resouces for the local player, add it to the buffer
             buffer = "Wool: " + ResDisplay(currentPlayer, StealableType.Resource_Wool) + "\n" +
                 "Lumber: " + ResDisplay(currentPlayer, StealableType.Resource_Lumber) + "\n" +
                 "Ore: " + ResDisplay(currentPlayer, StealableType.Resource_Ore) + "\n" +
                 "Brick: " + ResDisplay(currentPlayer, StealableType.Resource_Brick) + "\n" +
                 "Grain: " + ResDisplay(currentPlayer, StealableType.Resource_Grain) + "\n";
 
-            List<GameObject> connectedPlayers = GameManager.ConnectedPlayers;
+            // go through all connected players
             foreach (GameObject i in connectedPlayers)
             {
                 int count = 0;
+                // only do this for the connected players who aren't the local player
                 if (!i.Equals(currentPlayer))
                 {
+                    // add all their resources to count
                     foreach(KeyValuePair<StealableType, int> entry in i.GetComponent<GamePlayer>().playerResources)
                     {
                         count += entry.Value;
                     }
+                    // add the count to resource card count to the buffer
                     buffer += "\nName: " + i.GetComponent<GamePlayer>().myName + "\nResource Cards: " + count + "\n";
                 }
             }
-
-
-            /*
-			GetComponentInChildren<Text> ().text = 
-				"Wool: " + ResDisplay(currentPlayer, StealableType.Resource_Wool) + "\n" +
-				"Lumber: " + ResDisplay(currentPlayer, StealableType.Resource_Lumber) + "\n" + 
-				"Ore: " + ResDisplay(currentPlayer, StealableType.Resource_Ore) + "\n" + 
-				"Brick: " + ResDisplay(currentPlayer, StealableType.Resource_Brick) + "\n" + 
-				"Grain: " + ResDisplay(currentPlayer, StealableType.Resource_Grain) +
-                "\n\n" + ;
-                */
+            
+            // set the text's value to buffer
             GetComponentInChildren<Text>().text = buffer;
 		}
 
