01cf3623236e0e6a1264adca1345a9e164cac29d
abehnan <abehnan@gmail.com>
2017-03-20 21:45:53 -0400

    Fixed Resource Panel
    It now includes all players

--- Assets/Scripts/UI/PlayerResourcePanel.cs
+++ Assets/Scripts/UI/PlayerResourcePanel.cs
@@ -18,16 +18,46 @@ public class PlayerResourcePanel : MonoBehaviour {
 		}
 	}
 
+
 	// Update is called once per frame
 	void Update () {
+
 		if (GameManager.Instance.GameStateReadyAtStage (GameState.GameStatus.GRID_CREATED)) {
+
+            string buffer;
 			GamePlayer currentPlayer = GameManager.LocalPlayer.GetComponent<GamePlayer> ();
+
+            buffer = "Wool: " + ResDisplay(currentPlayer, StealableType.Resource_Wool) + "\n" +
+                "Lumber: " + ResDisplay(currentPlayer, StealableType.Resource_Lumber) + "\n" +
+                "Ore: " + ResDisplay(currentPlayer, StealableType.Resource_Ore) + "\n" +
+                "Brick: " + ResDisplay(currentPlayer, StealableType.Resource_Brick) + "\n" +
+                "Grain: " + ResDisplay(currentPlayer, StealableType.Resource_Grain) + "\n";
+
+            List<GameObject> connectedPlayers = GameManager.ConnectedPlayers;
+            foreach (GameObject i in connectedPlayers)
+            {
+                int count = 0;
+                if (!i.Equals(currentPlayer))
+                {
+                    foreach(KeyValuePair<StealableType, int> entry in i.GetComponent<GamePlayer>().playerResources)
+                    {
+                        count += entry.Value;
+                    }
+                    buffer += "\nName: " + i.GetComponent<GamePlayer>().myName + "\nResource Cards: " + count + "\n";
+                }
+            }
+
+
+            /*
 			GetComponentInChildren<Text> ().text = 
 				"Wool: " + ResDisplay(currentPlayer, StealableType.Resource_Wool) + "\n" +
 				"Lumber: " + ResDisplay(currentPlayer, StealableType.Resource_Lumber) + "\n" + 
 				"Ore: " + ResDisplay(currentPlayer, StealableType.Resource_Ore) + "\n" + 
 				"Brick: " + ResDisplay(currentPlayer, StealableType.Resource_Brick) + "\n" + 
-				"Grain: " + ResDisplay(currentPlayer, StealableType.Resource_Grain);
+				"Grain: " + ResDisplay(currentPlayer, StealableType.Resource_Grain) +
+                "\n\n" + ;
+                */
+            GetComponentInChildren<Text>().text = buffer;
 		}
 
 	}
