eb99cf20f6e8efa1bee9092b119647e6bf8f6ec3
arunench <arunench@gmail.com>
2017-03-26 23:16:55 -0400

    Added comments to UIHex and started Robber script
    Added comments to UIHex and started adding functions to Robber script.
    Also added a Robber sprite.

--- Assets/Scripts/RobberUnit.cs
+++ Assets/Scripts/RobberUnit.cs
@@ -4,13 +4,41 @@ using UnityEngine;
 
 public class RobberUnit : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+    public Vector2 HexGridPosition;
+    public Vec3 HexGridCubePosition;
+
+    // Use this for initialization
+    void Start () {
+       
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+
+        if (!GameManager.Instance.GameStateReadyAtStage(GameState.GameStatus.GRID_CREATED))
+        {
+            return;
+        }
+
+
+    }
+
+    public RobberUnit(Vector2 HexGridPosition, Vec3 HexGridCubePosition)
+    {
+        this.HexGridPosition = HexGridPosition;
+        this.HexGridCubePosition = HexGridCubePosition;
+    }
+
+    public void ActivateRobber()
+    {
+        if (!GameManager.Instance.GameStateReadyAtStage(GameState.GameStatus.GRID_CREATED))
+        {
+            return;
+        }
+
+        if (GameManager.Instance.GetCurrentGameState().CurrentTurn.IsLocalPlayerTurn())
+        {
+        }
+
+    }
 }
