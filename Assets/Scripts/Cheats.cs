5c5ea83502fbf1cb5b6c1f54e589d451bc1b3a4e
Dennis Kuang <denniskrq@hotmail.com>
2017-03-31 15:12:01 -0400

    See Desc for Detailed Changelog
    - Updated .gitignore
    - Folder structure organization
    - Removal of "UltimateFreeTexture" asset pack since it doesn't seem to
    be in use
    - Added an empty object called "CheatEngine" in Game, holding the
    "Cheats" script, for debug purposes (currently has press G to invoke the
    gold menu - see below)
    - Added gold menu: when a player rolls a gold field, the menu SHOULD
    popup (not implemented yet). The player selects a resource from it, and
    said resource is incremented by 1

---
+++ Assets/Scripts/Cheats.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// This is where hacky stuff used for debug goes
+public class Cheats : MonoBehaviour {
+
+    GameObject goldPopup;
+
+	// Use this for initialization
+	void Start () {
+        GameObject UICanvas = GameObject.Find("Canvas");
+        for (int i = 0; i < UICanvas.transform.childCount; i++)
+        {
+            if (UICanvas.transform.GetChild(i).name.Equals("GoldPopup"))
+            {
+                goldPopup = UICanvas.transform.GetChild(i).gameObject;
+            }
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // Press G to invoke the gold popup regardless of die roll
+		if (Input.GetKey(KeyCode.G))
+        {
+            goldPopup.SetActive(true);
+        }
+	}
+}
---
+++ Assets/Scripts/GoldPopup.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GoldPopup : MonoBehaviour {
+
+    public void accepted()
+    {
+        ToggleGroup toggles = gameObject.GetComponentInChildren<ToggleGroup>();
+        IEnumerator<Toggle> togglesEnum = toggles.ActiveToggles().GetEnumerator();
+        togglesEnum.MoveNext();
+        Toggle toggle = togglesEnum.Current;
+        string resourceSelected = toggle.transform.parent.name;
+        StealableType selected;
+
+        if (resourceSelected.Equals("Brick"))
+        {
+            selected = StealableType.Resource_Brick;
+        }
+        else if (resourceSelected.Equals("Grain"))
+        {
+            selected = StealableType.Resource_Grain;
+        }
+        else if (resourceSelected.Equals("Lumber"))
+        {
+            selected = StealableType.Resource_Lumber;
+        }
+        else if (resourceSelected.Equals("Ore"))
+        {
+            selected = StealableType.Resource_Ore;
+        }
+        else
+        {
+            selected = StealableType.Resource_Wool;
+        }
+
+        Debug.Log("Player selected: " + resourceSelected);
+
+        GamePlayer player = GameManager.LocalPlayer.GetComponent<GamePlayer>();
+        player.playerResources[selected]++;
+    }
+
+}
