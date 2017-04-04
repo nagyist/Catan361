eef97c919fbce24fc9b98370dc7ec3296a949a87
Jeremie Poisson <jeremie@weboxigen.com>
2017-04-04 14:41:00 -0400

    added tradebutton

---
+++ Assets/Scripts/UI/TradeButton.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TradeButton : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		int currentPlayerIndex = transform.GetComponentInParent<PlayerResourcePanel> ().PlayerIndex;
+		if (GameManager.ConnectedPlayers.IndexOf (GameManager.LocalPlayer) == currentPlayerIndex) {
+			GetComponent<Button> ().enabled = false;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
