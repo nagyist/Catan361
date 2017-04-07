7d0ef7a8d52387a3339a8a9608e3df536a07dc2b
Alex <amr_sauron@msn.com>
2017-04-06 21:16:10 -0400

    committing changes for maritime trade btn / menu / harbour ui

--- Assets/Scripts/Harbour.cs
+++ Assets/Scripts/Harbour.cs
@@ -7,12 +7,25 @@ public class Harbour : MonoBehaviour {
 	public int exchangeRate;
 	public StealableType returnedResource;
 	public int returnedAmount;
+	public Edge harbourEdge;
 
-	// Use this for initialization
-	void Start () {
-		
+	void OnMouseEnter(){
+		GetComponentInChildren<SpriteRenderer>().color = new Color32 (0, 255, 0, 255);
 	}
-	
+
+	void OnMouseExit(){
+		GetComponentInChildren<SpriteRenderer>().color = new Color32 (255, 255, 255, 255);
+	}
+
+	void OnMouseDown() {
+		/*
+		if(harbourEdge.)
+		{
+			launch popup window for trade
+		}
+		*/
+	}
+
 	// Update is called once per frame
 	void Update () {
 		
