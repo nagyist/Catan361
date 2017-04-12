ad816dd6d104aa2b95433f2ff7d1ee975d7e963b
Jeremie Poisson <jeremie@weboxigen.com>
2017-04-12 09:00:18 -0400

    added select player ui

---
+++ Assets/SelectPlayerEntry.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SelectPlayerEntry : MonoBehaviour {
+
+	public string PlayerName;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (PlayerName == GameManager.LocalPlayer.GetComponent<GamePlayer> ().myName) {
+			transform.FindChild ("Toggle").GetComponent<Toggle> ().enabled = false;
+		}
+
+		transform.FindChild ("Text").GetComponent<Text> ().text = PlayerName;
+	}
+}
